20af5df5273d8814bff32becc335d9da161c760a
Ihor Naidonov <superihor20@gmail.com>
2023-02-05 09:02:24 +0200

    head component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { MainLaoyut } from '../src/components/layouts/main/main';
 
 import '../src/assets/scss/global.scss';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MainLaoyut>
--- src/components/layouts/main/main.tsx
+++ src/components/layouts/main/main.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head';
+
 import { ComponentWithChildren } from '../../../types/component-with-children';
 
 export const MainLaoyut: ComponentWithChildren = ({ children }) => {
-  return <div className="bg-slate-100 min-h-screen p-8">{children}</div>;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-slate-100 min-h-screen p-8">{children}</div>
+    </>
+  );
 };
