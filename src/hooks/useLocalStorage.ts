9eb4f96da68680437c402bd08ed9d49367ebf808
Ihor Naidonov <superihor20@gmail.com>
2023-02-07 11:06:28 +0200

    refactoring uselocal

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,12 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { localStorageKeys } from '../utils/enums/local-storage-keys';
 
 export const useLocalStorage = <S>(
   key: localStorageKeys,
   initialValue?: S,
-): [S, (value: S) => void, boolean] => {
-  const [isLoading, setIsLoading] = useState(true);
+): [S, (value: S) => void] => {
   const [storedValue, setStoredValue] = useState<S>(() => {
     if (typeof window === 'undefined') {
       return initialValue;
@@ -23,9 +22,5 @@ export const useLocalStorage = <S>(
     window.localStorage.setItem(key, JSON.stringify(value));
   };
 
-  useEffect(() => {
-    setIsLoading(false);
-  }, []);
-
-  return [storedValue, setValue, isLoading];
+  return [storedValue, setValue];
 };
