0667ccea115fabcf8dcfe83e23b8cc01fe23fd3a
Ihor Naidonov <superihor20@gmail.com>
2023-02-09 16:13:14 +0200

    theme

--- src/components/admin/layouts/main.tsx
+++ src/components/admin/layouts/main.tsx
@@ -1,6 +1,5 @@
 import { BookOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { ConfigProvider, MenuProps, Breadcrumb, Layout, Menu, theme } from 'antd';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -27,26 +26,36 @@ export const MainLayoutAdmin: ComponentWithChildren = ({ children }) => {
   const breadcrumbItems = getBreadcrumbValues(router.pathname);
 
   return (
-    <Layout>
-      <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          {breadcrumbItems.map((breadcrumbItem) => (
-            <Breadcrumb.Item key={breadcrumbItem}>{breadcrumbItem}</Breadcrumb.Item>
-          ))}
-        </Breadcrumb>
-        <Layout style={{ padding: '24px 0', background: colorBgContainer }}>
-          <Sider style={{ background: colorBgContainer }} width={220}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={[adminPages.questionsCategories]}
-              style={{ height: '100%' }}
-              items={menuItems}
-            />
-          </Sider>
-          <Content style={{ padding: '0 24px', minHeight: 280 }}>{children}</Content>
-        </Layout>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ihor Naidonov ©2023 Created by Ihor Naidonov</Footer>
-    </Layout>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#34d399',
+        },
+      }}
+    >
+      <Layout>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            {breadcrumbItems.map((breadcrumbItem) => (
+              <Breadcrumb.Item key={breadcrumbItem}>{breadcrumbItem}</Breadcrumb.Item>
+            ))}
+          </Breadcrumb>
+          <Layout style={{ padding: '24px 0', background: colorBgContainer }}>
+            <Sider style={{ background: colorBgContainer }} width={220}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={[adminPages.questionsCategories]}
+                style={{ height: '100%' }}
+                items={menuItems}
+              />
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>{children}</Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ihor Naidonov ©2023 Created by Ihor Naidonov
+        </Footer>
+      </Layout>
+    </ConfigProvider>
   );
 };
