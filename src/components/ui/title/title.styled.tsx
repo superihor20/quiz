1a3af4a51fba3e269451c2913cf478f90ba94ebb
Ihor Naidonov <superihor20@gmail.com>
2023-02-16 13:07:32 +0200

    title component

--- src/components/ui/title/title.styled.tsx
+++ src/components/ui/title/title.styled.tsx
@@ -1,9 +1,11 @@
-import Title from 'antd/es/typography/Title';
+import { Typography } from 'antd';
 import styled from 'styled-components';
 
 export type TitleStyledProps = {
   align?: 'left' | 'center' | 'right';
 };
+
+const { Title } = Typography;
 
 export const TitleStyled = styled(Title)<TitleStyledProps>`
   text-align: ${({ align = 'left' }) => align};
