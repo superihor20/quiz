6a794a77316d2b8204b55ad6da0b747b86bb0b9d
Ihor Naidonov <superihor20@gmail.com>
2023-02-16 15:56:09 +0200

    sign up

--- src/components/screens/sign-up.tsx
+++ src/components/screens/sign-up.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button } from 'antd';
 import { Controller, useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
 
 import { authApi, UserCredentials } from '../../api';
 import { useLocalStorage } from '../../hooks/use-local-storage';
@@ -8,9 +8,12 @@ import { pages } from '../../utils/constans/links';
 import { localStorageKeys } from '../../utils/enums/local-storage-keys';
 import { CustomError } from '../../utils/helpers/get-error-message';
 import { userCredentialsSchema } from '../../utils/zod-schemas/user-credentials.schema';
+import { Button } from '../form/button';
 import { Form } from '../form/form';
 import { FormHelper } from '../form/form-helper';
 import { Input } from '../form/input';
+import { Paper } from '../ui/paper';
+import { Title } from '../ui/title';
 
 export const SignUp = () => {
   const [, setAccessToken] = useLocalStorage<null | string>(localStorageKeys.ACCESS_TOKEN, null);
@@ -22,34 +25,45 @@ export const SignUp = () => {
   } = useForm<UserCredentials>({
     resolver: zodResolver(userCredentialsSchema),
   });
+  const mutation = useMutation(authApi.signUp, {
+    onSuccess: ({ accessToken }) => {
+      setAccessToken(accessToken);
+    },
+    onError: (e) => {
+      setError('email', { message: (e as CustomError).message });
+    },
+  });
 
   const handleSubmit = async (data: UserCredentials) => {
-    try {
-      const { accessToken } = await authApi.signIn(data);
-
-      setAccessToken(accessToken);
-    } catch (e) {
-      setError('email', { message: (e as CustomError).message });
-    }
+    mutation.mutate(data);
   };
 
   return (
-    <div>
-      <h1>Sign up</h1>
+    <Paper>
+      <Title align="center">Sign up</Title>
       <Form onSubmit={handleSubmitHook((data) => handleSubmit(data))}>
         <Controller
           name="email"
           control={control}
-          render={({ field }) => <Input error={errors.email?.message} field={field} />}
+          render={({ field }) => (
+            <Input error={errors.email?.message} field={field} placeholder="Email" />
+          )}
         />
         <Controller
           name="password"
           control={control}
-          render={({ field }) => <Input error={errors.password?.message} field={field} />}
+          render={({ field }) => (
+            <Input
+              error={errors.password?.message}
+              field={field}
+              placeholder="Password"
+              type="password"
+            />
+          )}
         />
         <Button htmlType="submit">Submit</Button>
-        <FormHelper text="Already have an account?" link={{ text: 'Sign in', url: pages.signIn }} />
       </Form>
-    </div>
+      <FormHelper text="Already have an account?" link={{ text: 'Sign in', url: pages.signIn }} />
+    </Paper>
   );
 };
