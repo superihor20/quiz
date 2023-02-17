1319c9a661211c34b4c5e4dba5a7ef2322d28fa4
Ihor Naidonov <superihor20@gmail.com>
2023-02-17 13:41:10 +0200

    change styles for the link

--- src/components/admin/screens/questions-categories-screen/questions-categories-screen.styled.tsx
+++ src/components/admin/screens/questions-categories-screen/questions-categories-screen.styled.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { LinkStyled } from '@/ui/gradient-link/gradient-link.styled';
 
 export const NewCategoryLink = styled(LinkStyled)`
-  display: block;
+  display: inline-block;
   margin-bottom: 0.5em;
   font-size: 1.5em;
   line-height: 1.6em;
