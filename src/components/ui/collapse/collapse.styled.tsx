0dd69fed6a82c50d9792e5a39e6f8120bc6d0b61
Ihor Naidonov <superihor20@gmail.com>
2023-02-22 15:23:26 +0200

    style changes (collapse)

--- src/components/ui/collapse/collapse.styled.tsx
+++ src/components/ui/collapse/collapse.styled.tsx
@@ -4,5 +4,4 @@ import styled from 'styled-components';
 export const CollapseOuterWrapper = styled.div`
   background: ${grey[7]};
   border-radius: 10px;
-  cursor: pointer;
 `;
--- src/components/ui/collapse/components/header/header.styled.tsx
+++ src/components/ui/collapse/components/header/header.styled.tsx
@@ -6,5 +6,6 @@ export const HeaderStyled = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 1rem;
+  cursor: pointer;
   box-shadow: 0 1px 10px rgba(0, 0, 0, 0.5);
 `;
