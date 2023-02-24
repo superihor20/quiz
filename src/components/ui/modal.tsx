73897b5f43b9dc4b4e7d3399ac13da8863cba5eb
Ihor Naidonov <superihor20@gmail.com>
2023-02-24 13:43:50 +0200

    modal component

---
+++ src/components/ui/modal.tsx
@@ -0,0 +1,47 @@
+import { Modal as ModalAntd } from 'antd';
+import { useState } from 'react';
+
+import { Button } from '@/form/button';
+import { ComponentWithChildren } from '@/types/component-with-children';
+
+type ModalProps = ComponentWithChildren<{
+  buttonText: string;
+  title: string;
+}>;
+
+export const Modasl: ModalProps = ({ buttonText, title, children }) => {
+  const [open, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+  };
+
+  const handleOk = () => {
+    showModal();
+  };
+
+  const handleClose = () => {
+    closeModal();
+  };
+
+  return (
+    <>
+      <Button htmlType="button" onClick={showModal}>
+        {buttonText}
+      </Button>
+      <ModalAntd
+        title={title}
+        open={open}
+        onOk={handleOk}
+        confirmLoading={false}
+        onCancel={handleClose}
+      >
+        {children}
+      </ModalAntd>
+    </>
+  );
+};
